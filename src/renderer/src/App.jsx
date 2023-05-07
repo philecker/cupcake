7a109f94e59b14abee33db3e20e93e73a2d2d9d4
Phil Ecker <hello@philecker.com>
2023-05-07 11:50:36 -0500

    Begin adding update to the last restart time

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import copyIcon from './assets/copy.png'
 import store  from 'store'
 
-const systemDetails = store.get('cupcakeInfo');
-console.log(systemDetails);
+const [cupcakeInfo, setCupcakeInfo] = useState(null);
+
+useEffect(() => {
+  function checkUserData() {
+    const systemDetails = store.get('cupcakeInfo');
+    setCupcakeInfo(systemDetails);
+  }
+
+  window.addEventListener('storage', checkUserData)
+
+  return () => {
+    window.removeEventListener('storage', checkUserData)
+  }
+}, [])
 
 function App() {
   return (
@@ -10,25 +23,25 @@ function App() {
       <li>
         <div className="cupcake-label">
           Internal IP: <img className="copyIcon" src={copyIcon} />
-          <span id="ip-address">{ systemDetails.cupcakeIp }</span>
+          <span id="ip-address">{ cupcakeInfo.cupcakeIp }</span>
         </div>
       </li>
       <li>
         <div className="cupcake-label">
           Username: <img className="copyIcon" src={copyIcon} />
-          <span id="computer-name">{ systemDetails.cupcakeUsername }</span>
+          <span id="computer-name">{ cupcakeInfo.cupcakeUsername }</span>
         </div>
       </li>
       <li>
         <div className="cupcake-label">
           OS Version: <img className="copyIcon" src={copyIcon} />
-          <span id="os-version">{ systemDetails.cupcakeOsVersion }</span>
+          <span id="os-version">{ cupcakeInfo.cupcakeOsVersion }</span>
         </div>
       </li>
       <li>
         <div className="cupcake-label">
           Last Restart: <img className="copyIcon" src={copyIcon} />
-          <span id="last-restart">{ systemDetails.cupcakeUptime }</span>
+          <span id="last-restart">{ cupcakeInfo.cupcakeUptime }</span>
         </div>
       </li>
     </ul>
